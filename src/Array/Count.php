08ac85c2b572d65678346f70afe60195bfe87b21
Anton Panfilov <anton@panfilov.biz>
2025-03-03 20:20:38 -0800

    alpha version - checkpoint

--- src/Array/Count.php
+++ src/Array/Count.php
@@ -1,9 +1,8 @@
 <?php
 
-namespace AP\Validator\Int;
+namespace AP\Validator\Array;
 
 use AP\ErrorNode\Errors;
-use AP\Validator\Array\AbstractArray;
 use Attribute;
 
 #[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD | Attribute::TARGET_PROPERTY)]
