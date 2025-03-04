4481de6ef470e2e18babb8aac242f791f394c191
Anton Panfilov <anton@panfilov.biz>
2025-03-04 13:06:10 -0800

    add validate by scheme validation implemented objects

--- src/Object/ObjectValidator.php
+++ src/Object/ObjectValidator.php
@@ -4,34 +4,48 @@ namespace AP\Validator\Object;
 
 
 use AP\ErrorNode\Errors;
+use AP\Scheme\Validation;
 use AP\Validator\ValidatorInterface;
 use ReflectionClass;
-use ReflectionException;
 
 class ObjectValidator extends AbstractObject
 {
     /**
-     * @param object $value
+     * @param object $obj
      * @param array $path
      * @return true|Errors
-     * @throws ReflectionException
      */
-    final public function validateObject(mixed &$obj, array $path = []): true|Errors
+    final public function validateObject(object &$obj, array $path = []): true|Errors
     {
         $errors = [];
 
         $reflection = new ReflectionClass($obj);
 
         foreach ($reflection->getProperties() as $property) {
-            foreach ($property->getAttributes() as $attribute) {
-                if (!$property->isPublic()) {
-                    continue;
-                }
-                $name = $property->getName();
-                if ($property->getType()->allowsNull() && is_null($obj->$name)) {
-                    continue;
-                }
+            // skip no public
+            if (!$property->isPublic()) {
+                continue;
+            }
 
+            // skip null if allow null
+            $name = $property->getName();
+            if ($property->getType()->allowsNull() && is_null($obj->$name)) {
+                continue;
+            }
+
+            // element implement Validation scheme
+            if ($obj->$name instanceof Validation) {
+                $validateRes = $obj->$name->isValid($obj->$name);
+                if ($validateRes instanceof Errors) {
+                    foreach ($validateRes->getErrors() as $error) {
+                        $error->path = array_merge($path, [$name], $error->path);
+                        $errors[]    = $error;
+                    }
+                }
+            }
+
+            // validation attributes
+            foreach ($property->getAttributes() as $attribute) {
                 if (is_subclass_of($attribute->getName(), ValidatorInterface::class)) {
                     $validator = $attribute->newInstance();
                     if ($validator instanceof ValidatorInterface) {
