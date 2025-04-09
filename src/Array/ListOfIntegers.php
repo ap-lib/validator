55a7c46fe5d3a21188edfc153c07267f31fb755b
Anton Panfilov <anton@panfilov.biz>
2025-04-09 10:27:36 -0700

    rename validator TypeInt to ListOfIntegers

--- src/Array/ListOfIntegers.php
+++ src/Array/ListOfIntegers.php
@@ -6,7 +6,7 @@ use AP\ErrorNode\Errors;
 use Attribute;
 
 #[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD | Attribute::TARGET_PROPERTY)]
-class TypeInt extends AbstractArray
+class ListOfIntegers extends AbstractArray
 {
     public function __construct(
         public ?int   $min = null,
