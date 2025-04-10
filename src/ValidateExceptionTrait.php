7a816a4dfdd912afbbbc54d1b539e68e8bfb1221
Anton Panfilov <anton@panfilov.biz>
2025-04-10 15:19:40 -0700

    ValidateExceptionTrait bug fix str -> mixed

--- src/ValidateExceptionTrait.php
+++ src/ValidateExceptionTrait.php
@@ -7,9 +7,9 @@ use AP\ErrorNode\Errors;
 
 trait ValidateExceptionTrait
 {
-    public function validateException(string &$str): void
+    public function validateException(mixed &$value): void
     {
-        $res = $this->validate($str);
+        $res = $this->validate($value);
         if ($res instanceof Errors) {
             throw $res->getNodeErrorsThrowable();
         }
