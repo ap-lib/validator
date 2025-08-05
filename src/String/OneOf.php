b70a36e22ce1deff31d2e436468c504672f32443
Anton Panfilov <anton@panfilov.biz>
2025-08-05 12:20:26 -0700

    One off for strings

---
+++ src/String/OneOf.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace AP\Validator\String;
+
+use AP\ErrorNode\Errors;
+use AP\Validator\ValidatorOpenAPIInterface;
+use Attribute;
+
+#[Attribute(Attribute::IS_REPEATABLE | Attribute::TARGET_METHOD | Attribute::TARGET_PROPERTY)]
+class OneOf extends AbstractString implements ValidatorOpenAPIInterface
+{
+    public function __construct(
+        public array  $options,
+        public string $message = "must be one of: {options}'",
+    )
+    {
+    }
+
+    public function validateString(string &$str): true|Errors
+    {
+        if (!in_array($str, $this->options, true)) {
+            return Errors::one($this->message, [
+                "options" => implode(
+                    ", ",
+                    array_map(
+                        fn($v) => var_export($v, true),
+                        $this->options
+                    )
+                ),
+            ]);
+        }
+
+        return true;
+    }
+
+    public function updateOpenAPIElement(array $spec): array
+    {
+        $spec['enum'] = $this->options;
+        return $spec;
+    }
+}
